abec6361455fc93d68546888f87c847ac5cd0685
seifzadeh <mehrdad.seyfzadeh@gmail.com>
2016-03-16 14:02:10 +0330

    Create loading-so.c

---
+++ loading-so.c
@@ -0,0 +1,4 @@
+https://stackoverflow.com/questions/384121/creating-a-module-system-dynamic-loading-in-c/384337#384337?newreg=7b56cbcf23504aca92d4fc535fd2adf6
+http://www.linuxjournal.com/article/3687
+http://linux.die.net/man/3/dlopen
+http://c.learncodethehardway.org/book/ex29.html
